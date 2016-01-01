5c7542b52fc8fa9806b54ea44a1fa0954c53d4f1
Iulian Tabără <itabara@live.com>
2016-01-01 15:54:45 +0200

    Adding blogPosts.js collection

---
+++ blogPosts.js
@@ -0,0 +1,72 @@
+db.blogPosts.insert(
+    {
+        title:'Intro to MongoDB',
+        author: 'Iulian',
+        url:'http://www.iuliantabara.com',
+        tags:['noSQL', 'documents', 'Big Data'],
+        likes: 120
+    }   
+);
+
+db.blogPosts.insert(
+    {
+        title:'Intro to MongoDB Indexes',
+        author: 'Emilian',
+        url:'http://www.iuliantabara.com',
+        tags:['noSQL', 'indexes', 'Big Data'],
+        likes: 130
+    }   
+);
+
+db.blogPosts.insert(
+    {
+        title:'Intro to Aggregation Framework on MongoDB',
+        author: 'Emilian',
+        url:'http://www.iuliantabara.com',
+        tags:['noSQL', 'documents', 'Aggregation'],
+        likes: 90
+    }   
+);
+
+db.blogPosts.insert(
+    {
+        title:'MongoDB CRUD',
+        author: 'Iulian',
+        url:'http://www.iuliantabara.com',
+        tags:['noSQL', 'documents', 'Big Data'],
+        likes: 150
+    }   
+);
+
+db.blogPosts.insert(
+    {
+        title:'MongoDB Performance Tunning',
+        description: 'Introduction to Performance Tunning',
+        author: 'Iulian',
+        url:'http://www.iuliantabara.com',
+        tags:['noSQL', 'documents', 'Big Data'],
+        likes: 90
+    }   
+);
+
+db.blogPosts.insert(
+    {
+        title:'MongoDB Creating Index',
+        description: 'Creating Index on MongoDB',
+        author: 'Emilian',
+        url:'http://www.iuliantabara.com',
+        tags:['noSQL', 'documents', 'Big Data'],
+        likes: 90
+    }   
+);
+
+db.blogPosts.insert(
+    {
+        title:'Regular Expressions in MongoDB',
+        description: 'Regular Expressions',
+        author: 'Emilian',
+        url:'http://www.iuliantabara.com',
+        tags:['noSQL', 'documents', 'Big Data'],
+        likes: 250
+    }   
+);
